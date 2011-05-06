8c799fb2110168c45d6097b31592324b34268a27
Luciano Ruete <lruete@sequre.com.ar>
2011-05-06 11:23:05 -0300

    Cronjobs are now handled with whenever

--- config/environment.rb
+++ config/environment.rb
@@ -37,7 +37,7 @@ Rails::Initializer.run do |config|
   config.gem 'fastercsv', :version => '~>1.5'
   config.gem 'aegis', :version => '~>2.5'
   config.gem 'acts_as_audited', :version => '~>1.1'
-
+  config.gem 'whenever', :version => '~>0.6'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
