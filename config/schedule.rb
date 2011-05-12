7405225751d2c9e7950efb32b00f21ea7eddb43b
Luciano Ruete <lruete@sequre.com.ar>
2011-05-12 17:56:09 -0300

    Relative location in logfile path (to be fixed)

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
 # http://github.com/javan/whenever
-set :output, "log/cron_log.log"
+set :output, "../log/cron_log.log"
 
 every 5.minutes do
   command "cd /opt/sequreisp/deploy/current/bin/ && ./sequreisp_rrd_feed.sh"
