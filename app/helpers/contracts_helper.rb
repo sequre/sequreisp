55b8b16f28de6f34798368c3370b68ca91583447
Luciano Ruete <lruete@sequre.com.ar>
2011-06-29 10:31:52 -0300

    Allow to search contracts by client.external_client_number

--- app/helpers/contracts_helper.rb
+++ app/helpers/contracts_helper.rb
@@ -16,17 +16,21 @@
 # along with Sequreisp.  If not, see <http://www.gnu.org/licenses/>.
 
 module ContractsHelper
-  def client_name_plus_detail_cpe_node_label
+  def client_name_plus_external_client_number_and_detail_cpe_node_label
     "#{t('activerecord.attributes.client.name')} " +
+    "| #{t('activerecord.attributes.client.external_client_number')} " +
     "(#{t('activerecord.attributes.contract.detail')}/" +
     "#{t('activerecord.attributes.contract.cpe')}/" +
     "#{t('activerecord.attributes.contract.node')})"
   end
-  def detail_cpe_node(contract)
-    if not contract.detail.blank? or not contract.cpe.blank? or not contract.node.blank?
-      "(#{contract.detail}/#{contract.cpe}/#{contract.node})"
-    else
-      ""
+  def external_client_number_and_detail_cpe_node(contract)
+    value = ""
+    if not contract.client.external_client_number.blank?
+      value += "| #{contract.client.external_client_number} "
     end
+    if not contract.detail.blank? or not contract.cpe.blank? or not contract.node.blank?
+      value += "(#{contract.detail}/#{contract.cpe}/#{contract.node})"
+    end
+    value
   end
 end
