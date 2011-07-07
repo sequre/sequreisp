477ea14d6a08f7483bc665c2b9714684b13bbdfb
Rodrigo Pavano <rpavano@sequre.com.ar>
2011-07-07 11:44:31 -0300

    Modified ModelsWatcher so it can call watch_fields several times

--- lib/models_watcher.rb
+++ lib/models_watcher.rb
@@ -4,6 +4,7 @@ module ModelsWatcher
     base.send(:extend, ClassMethods)
     base.send(:include, InstanceMethods)
     base.class_eval do
+      @__watched_fields ||= []
       before_save :check_watched_fields
     end
   end
@@ -11,13 +12,7 @@ module ModelsWatcher
   module ClassMethods
 
     def watch_fields(*fields)
-      define_method :check_watched_fields do
-        fields.each do |field|
-          if self.send "#{field}_changed?"
-            update_changes_to_apply
-          end
-        end
-      end
+      @__watched_fields += fields
     end
 
     def watch_on_destroy
@@ -27,6 +22,17 @@ module ModelsWatcher
   end
 
   module InstanceMethods
+
+    def check_watched_fields
+      fields = self.class.instance_eval do @__watched_fields end
+      fields.each do |field|
+        if self.send "#{field}_changed?"
+          update_changes_to_apply
+          break
+        end
+      end
+    end
+
     def update_changes_to_apply
       Configuration.first.update_attribute(:changes_to_apply, true)
     end
