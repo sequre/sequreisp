3eeed7d037594527857da0351a3f56e690f5bdf9
Luciano Ruete <lruete@sequre.com.ar>
2011-07-13 16:41:10 -0300

    More sleep because some drivers update /sys...statistics just once per second or more...

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -305,10 +305,10 @@ class Provider < ActiveRecord::Base
     Rails.logger.debug "Provider::is_online_by_rate? #{Time.now} provider_id: #{id} start"
     rx = interface.rx_bytes
     tx = interface.tx_bytes
-    sleep 0.5
-    #to kbps is: 0.5 * 2 is 1 second and 1024 bits are one kbit
-    instant_rate_down = (interface.rx_bytes-rx)*8*2/1024
-    instant_rate_up = (interface.tx_bytes-tx)*8*2/1024
+    sleep 2
+    # from bytes to bits(*8) to bps(/2) to kbps(/1024)
+    instant_rate_down = (interface.rx_bytes-rx)*8/2/1024
+    instant_rate_up = (interface.tx_bytes-tx)*8/2/1024
     # min rates in kbps
     min_online_rate_down = 56
     min_online_rate_up = 5
