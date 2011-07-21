371a70ab5886631630c85c660f9752e4315b22eb
Luciano Ruete <lruete@sequre.com.ar>
2011-07-21 12:30:48 -0300

    Add Configuration.iptables_tree_optimization_enabled to ModelsWatcher

--- app/models/configuration.rb
+++ app/models/configuration.rb
@@ -28,7 +28,8 @@ class Configuration < ActiveRecord::Base
                :mtu, :quantum_factor, :nf_conntrack_max, :gc_thresh1, :gc_thresh2, :gc_thresh3,
                :transparent_proxy, :transparent_proxy_n_to_m, :transparent_proxy_zph_enabled,
                :tc_contracts_per_provider_in_lan, :tc_contracts_per_provider_in_wan,
-               :filter_by_mac_address, :clamp_mss_to_pmtu, :use_global_prios
+               :filter_by_mac_address, :clamp_mss_to_pmtu, :use_global_prios,
+               :iptables_tree_optimization_enabled
 
   validates_presence_of :default_tcp_prio_ports, :default_prio_protos, :default_prio_helpers, :mtu, :quantum_factor, :nf_conntrack_max, :gc_thresh1, :gc_thresh2, :gc_thresh3
   validates_presence_of :notification_email, :if => Proc.new { |c| c.deliver_notifications? }
