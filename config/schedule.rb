bee234a43ce615aab98e86e61ccc52522db85fe5
Luciano Ruete <lruete@sequre.com.ar>
2011-08-23 11:05:06 -0300

    Inline output definition

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,7 @@
 # http://github.com/javan/whenever
-set :output, "../log/cron_log.log"
 
 every 5.minutes do
-  command "cd /opt/sequreisp/deploy/current/bin/ && ./sequreisp_rrd_feed.sh"
+  command "cd /opt/sequreisp/deploy/current/bin/ && ./sequreisp_rrd_feed.sh", :output => "../log/cron_log.log"
 end
 
 
