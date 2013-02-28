e63dc9358ddff500b65b4cc699d3ced260fdb4a0
Luciano Ruete <lruete@sequre.com.ar>
2013-02-28 14:29:13 -0300

    Fix identation

--- db/migrate/20130124150324_add_start_date_to_contract.rb
+++ db/migrate/20130124150324_add_start_date_to_contract.rb
@@ -1,9 +1,9 @@
 class AddStartDateToContract < ActiveRecord::Migration
   def self.up
-  	add_column :contracts, :start_date, :date
+    add_column :contracts, :start_date, :date
   end
 
   def self.down
-  	remove_column :contracts, :start_date
+    remove_column :contracts, :start_date
   end
 end
