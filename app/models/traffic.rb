ea4c42cdcb7d08c6fb92a24b322367049f24689e
g.edera <gab.edera@gmail.com>
2013-06-13 14:06:15 -0300

    delete logic for increse extra data total

--- app/models/traffic.rb
+++ app/models/traffic.rb
@@ -2,16 +2,10 @@ class Traffic < ActiveRecord::Base
 
   belongs_to :contract
 
-  before_update :accumulate_data_total_extra
-
   named_scope :currents, :conditions => ["from_date <= ? and to_date >= ?", Date.today, Date.today]
 
   named_scope :for_date, lambda {|date| { :conditions => ["from_date >= ?", date]}}
 
   named_scope :for_contract, lambda {|id| { :conditions => (["contract_id = ?", id])}}
-
-  def accumulate_data_total_extra
-    self.data_total_extra +=  self.data_total_extra_was
-  end
 
 end
