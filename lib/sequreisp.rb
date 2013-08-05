4fd79d0035310807baffb62457d134987f021937
Luciano Ruete <lruete@sequre.com.ar>
2013-08-05 10:19:19 -0300

    Allow dns queries from LAN

--- lib/sequreisp.rb
+++ lib/sequreisp.rb
@@ -634,6 +634,10 @@ def gen_iptables
       f.puts "-A INPUT -i lo -j ACCEPT"
       f.puts "-A OUTPUT -o lo -j ACCEPT"
       f.puts "-A INPUT -p tcp --dport 3128 -j sequreisp-enabled"
+      Interface.all(:conditions => "kind = 'lan'").each do |i|
+        f.puts "-A INPUT -i #{i.name} -p udp --dport 53 -j ACCEPT"
+        f.puts "-A INPUT -i #{i.name} -p tcp --dport 53 -j ACCEPT"
+      end
       Provider.enabled.with_klass_and_interface.each do |p|
         if p.allow_dns_queries
           f.puts "-A INPUT -i #{p.link_interface} -p udp --dport 53 -j ACCEPT"
