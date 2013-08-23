c3e767e7c73b98bd3de1937eff5413ff7b8b7f7e
g.edera <gab.edera@gmail.com>
2013-08-23 15:11:57 -0300

    optimization delete traffic duplicate

--- script/destroy_traffic_duplicate_for_each_contract.rb
+++ script/destroy_traffic_duplicate_for_each_contract.rb
@@ -1,16 +1,13 @@
-Contract.all(:include => :traffics).each do |c|
+Contract.all.each do |c|
   unless c.traffics.empty?
     init = c.traffics.first.from_date
     final = c.traffics.last.to_date
     while init <= final
-      last = init.end_of_month
-      traffics = []
-      c.traffics.each do |t|
-        traffics << t if t.from_date >= init and t.to_date <= last
+      traffic = Traffic.first(:conditions => ["from_date = '#{init.strftime('%Y-%m-%d')}' and contract_id = #{c.id}"])
+      unless traffic.nil?
+        Traffic.delete_all(["to_date = '#{traffic.to_date.strftime('%Y-%m-%d')}' and from_date = '#{traffic.from_date.strftime('%Y-%m-%d')}' and contract_id = #{c.id} and id != #{traffic.id}"])
       end
-      traffics.shift
-      traffics.collect(&:delete)
-      init = init + 1.month
+      init = (init + 1.month).beginning_of_month
     end
   end
 end
