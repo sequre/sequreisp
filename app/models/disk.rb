aee36c847f5ff1707a9feab819b04a52d2a005f3
g.edera <gab.edera@gmail.com>
2013-11-27 11:38:47 -0300

    refactor method cache dir lines in model disk

--- app/models/disk.rb
+++ app/models/disk.rb
@@ -13,95 +13,22 @@ class Disk < ActiveRecord::Base
   named_scope :prepared_for_cache, :conditions => 'prepare_disk_for_cache = 1'
 #  before_update :clean_cache, :if => "self.free_changed? and self.free"
 
-  def is_mounted?
-    system("mount | grep '#{name}' &>/dev/null")
-  end
-
-  def self.scan
-    disks = {}
-    aux =`sudo /usr/bin/lshw -C disk`.strip.split("*-")
-    system_disks = used_for_system
-    cache_disks = used_for_cache
-
-    aux.each do |disk|
-      if disk.include?("disk")
-        name = ""
-        capacity = ""
-        serial = ""
-        attributes = disk.split("  ")
-        attributes.each do |attr|
-          name = attr.chomp.split(":").last.strip if attr.include?("logical name")
-          capacity = attr.chomp.split(":").last.split(" ").last.strip if attr.include?("size:")
-          serial = attr.chomp.split(":").last.strip if attr.include?("serial")
-        end
-        is_system = system_disks[:devices].include?(name) ? true : false
-        is_cache = cache_disks[:devices].include?(name) ? true : false
-        #which_raid = system_disks[:raid] if is_system
-        #which_raid = cache_disks[:raid] if is_cache
-        is_free = is_system or is_cache ? false : true
-        partitioned =  is_free ? false : true
-        hash = {:name => name, :capacity => capacity, :serial => serial, :system => is_system, :cache => is_cache, :free => is_free, :partitioned => partitioned, :clean_partition => is_free}
-  #      scan_for_other_uses(hash)
-        hash[:raid] = `cat /proc/mdstat | grep "#{hash[:name].split('/').last}"`.chomp.split.first
-        disks[name] = hash
-      end
-    end
-    disks
-  end
-
   def self.new_scan
     devices = `lsscsi | grep disk`.split("\n").collect{ |x| x.split(" ").last }
     devices.each do |dev|
       disk = Disk.find_by_name(dev)
-
-      # if not disk.nil?
-      #   disk.serial = `sudo /sbin/blkid #{disk.partition_name_or_raid_name}`.chomp.split[1].split("=")[1].delete("\"") rescue nil
-      #   if disk.serial_changed?
-      #     disk.capacity = `fdisk -l | grep /dev/sda:`.split(" ")[4]
-      #     disk.raid = disk.which_raid
-      #     disk.free = true
-      #     disk.save
-      #   end
-      # else
-      #   disk = Disk.new if disk.nil?
-      #   disk.name = dev
-      #   disk.capacity = `fdisk -l | grep /dev/sda:`.split(" ")[4]
-      #   disk.serial = `sudo /sbin/blkid #{disk.partition_name_or_raid_name}`.chomp.split[1].split("=")[1].delete("\"") rescue nil
-      #   disk.raid = disk.which_raid
-      #   disk.is_used_for
-      #   disk.save
-      # end
-
       disk = Disk.new if disk.nil?
-      disk.name = dev
-      disk.capacity = `fdisk -l | grep /dev/sda:`.split(" ")[4]
-      disk.serial = `sudo /sbin/blkid #{disk.partition_name_or_raid_name}`.chomp.split[1].split("=")[1].delete("\"") rescue nil
-      disk.raid = disk.which_raid
-      disk.is_used_for
-      disk.save
-    end
-  end
 
-  def self.scaan
-    command_disks = `sudo /usr/bin/lshw -C disk`.strip.split("*-")
-    command_disks.each do |c_disk|
-      name = ""
-      capacity = ""
-      if c_disk.include?("disk")
-        attributes = c_disk.split("  ")
-        attributes.each do |attr|
-          name = attr.chomp.split(":").last.strip if attr.include?("logical name")
-          capacity = attr.chomp.split(":").last.split(" ").last.strip.delete("()") if attr.include?("size:")
-        end
-        if name.present?
-          disk = Disk.find_by_name(name)
-          disk = Disk.create(:name => name, :capacity => capacity) if disk.nil?
-          disk.raid = disk.which_raid
-          disk.is_used_for
-          disk.serial = `sudo /sbin/blkid #{disk.partition_name_or_raid_name}`.chomp.split[1].split("=")[1].delete("\"")
-          disk.save
-        end
+      disk.name = dev
+      disk.raid = disk.which_raid
+      disk.capacity = `fdisk -l | grep #{disk.name}:`.split(" ")[4]
+      disk.serial = `sudo /sbin/blkid #{disk.name_with_partition}`.chomp.split[1].split("=")[1].delete("\"") rescue nil
+      if disk.new_record?
+        disk.is_used_for
+      else
+        disk.assigned_for([:free]) if disk.serial_changed?
       end
+      disk.save
     end
   end
 
@@ -113,10 +40,6 @@ class Disk < ActiveRecord::Base
 
   def which_raid
     `cat /proc/mdstat | grep "#{self.logical_name}"`.split(" ").first
-  end
-
-  def partition_name_or_raid_name
-    raid.nil? ? "#{name}1" : "/dev/#{raid}"
   end
 
   def is_system_disk?
@@ -140,54 +63,14 @@ class Disk < ActiveRecord::Base
     is_cache
   end
 
-  def self.used_for_system
-    self.disk_usage("on / ")
-  end
-
-  def self.used_for_cache
-    hash = {:raid => nil, :devices => []}
-    devs = self.disk_usage("/mnt/sequreisp/dev")
-    if devs[:devices].empty?
-      devs = self.disk_usage("/mnt/cache")
-      devs = self.disk_usage("/mnt/cache/web") if devs[:devices].empty?
-      hash[:devices] = devs[:devices]
-    else
-      devs[:devices].each do |dev|
-        hash[:devices] << dev if File.directory?("/mnt/sequreisp#{dev}/squid")
-      end
-    end
-    hash[:raid] = devs[:raid]
-    hash
-  end
-
-  def self.disk_usage(command)
-    hash = {:raid => nil, :devices => []}
-    IO.popen("mount | grep '#{command}'", "r") do |io|
-      io.each do |line|
-        device = line.chomp.split(" ").first
-        if device.include?("md")
-          hash[:raid] = device
-          IO.popen("cat /proc/mdstat | grep #{device.split("/").last}", "r") do |io|
-            io.each do |line|
-              _cache_disks = line.chomp.split(" ")
-              _cache_disks[4.._cache_disks.count].each do |disk|
-                hash[:devices] << "/dev/#{disk[0..2]}"
-              end
-            end
-          end
-        else
-          hash[:devices] << device.delete(device.last)
-        end
-      end
-    end
-    hash
+  def is_mounted?
+    system("mount | grep '#{name}' &>/dev/null")
   end
 
   def assigned_for(attr)
     self.free   = attr.include?(:free) ? true : false
     self.system = attr.include?(:system) ? true : false
     self.cache  = attr.include?(:cache) ? true : false
-    self.save
   end
 
   def logical_name
@@ -197,10 +80,15 @@ class Disk < ActiveRecord::Base
   def name_with_partition
     "#{name}1"
   end
-  def mounting_point
-    "/mnt/sequreisp#{name}"
-    "/var/spool" if system
+
+  def partition_name_or_raid_name
+    raid.nil? ? "#{name}1" : "/dev/#{raid}"
   end
+
+  def mounting_point
+    system? ? "/var/spool" : "/mnt/sequreisp#{name}"
+  end
+
   def mount_and_add_to_fstab
     fstab_line = "#{name_with_partition} #{mounting_point} ext4 defaults 0 1"
     if system "grep '#{name_with_partition}' /etc/fstab"
@@ -211,15 +99,7 @@ class Disk < ActiveRecord::Base
     system "mkdir -p #{mounting_point}"
     system "mount #{name_with_partition}"
   end
-  def do_prepare_disk_for_cache
-    if system?
-      system "mkdir -p /var/spool/squid"
-      system "chown proxy.proxy -R /var/spool/squid" if `ls -l /var/spool | grep squid`.chomp.split[2] != "proxy"
-    else
-      system "mkdir -p #{mounting_point}/squid"
-      system "chown proxy.proxy -R #{mounting_point}/squid" if `ls -l #{mounting_point} | grep squid`.chomp.split[2] != "proxy"
-    end
-  end
+
   def umount_and_remove_from_fstab
     system "sed -i '@#{name_with_partition}@d' /etc/fstab"
     system "umount -l #{name_with_partition}"
@@ -230,12 +110,13 @@ class Disk < ActiveRecord::Base
     system "(echo n; echo p; echo 1; echo ; echo ; echo w) | fdisk #{name}"
     system "mkfs.ext4 #{name_with_partition}"
     # Save the new Partition UUID
-    serial = `sudo /sbin/blkid #{partition_name_or_raid_name}`.chomp.split[1].split("=")[1].delete("\"")
-    self.save
+    serial = `sudo /sbin/blkid #{name_with_partition}`.chomp.split[1].split("=")[1].delete("\"") rescue nil
+    save
   end
 
-  def self.not_custom_raids_present?
-    all(:conditions => 'raid is not NULL and system = 0').count == 0
+  def do_prepare_disk_for_cache
+    system "mkdir -p #{mounting_point}/squid"
+    system "chown proxy.proxy -R #{mounting_point}/squid" if `ls -l #{mounting_point} | grep squid`.chomp.split[2] != "proxy"
   end
 
   def partition_capacity
@@ -243,33 +124,152 @@ class Disk < ActiveRecord::Base
     @_capacity ||= `sudo /sbin/fdisk -l | grep 'Disk #{dev}'`.chomp.split(" ")[4].to_i / (1024 * 1024) * 0.30 #MEGABYTE
   end
 
+  def self.not_custom_raids_present?
+    all(:conditions => 'raid is not NULL and system = 0').count == 0
+  end
+
   MAX_SQUID_TOTAL_SIZE = 300*1024 #300GB
   MAX_SQUID_ON_SYSTEM_DISK_SIZE = 50 * 1024 #50GB
 
   def self.cache_dir_lines
     lines = []
+    system "sed -i '/^ *cache_dir*/ c #cache_dir ufs \/var\/spool\/squid 30000 16 256' /etc/squid/squid.conf"
     if Disk.not_custom_raids_present?
-      # NO RAIDS, whe take CONTROL
-      system "sed -i '/^ *cache_dir*/ c #cache_dir ufs \/var\/spool\/squid 30000 16 256' /etc/squid/squid.conf"
-
       cache_disks = Disk.cache
-      if cache_disks.empty?
-        #disk = Disk.system.first
-        #disk.do_prepare_disk_for_cache
-        # disco sistema: hasta 30% y un tope de 50 GB
-        value_for_cache_dir = disk.partition_capacity > MAX_SQUID_ON_SYSTEM_DISK_SIZE ? MAX_SQUID_ON_SYSTEM_DISK_SIZE : disk.partition_capacity
-        lines << "cache_dir aufs /var/spool/squid #{value_for_cache_dir.to_i} 16 256"
-      else
-        #system "rm -rf /var/spool/squid &"
-        # total_capacity = cache_disks.collect{|c| c.capacity.to_i}.sum / 1024 / 1024
-        total_capacity = cache_disks.collect{|c| c.partition_capacity.to_i}.sum
-        cache_disks.each do |disk|
-          # disco aparte: hasta 30% proporcional al disco hasta un tope de 300GB TOTAL
-          value_for_cache_dir =  total_capacity > MAX_SQUID_TOTAL_SIZE ? (disk.partition_capacity * MAX_SQUID_TOTAL_SIZE / total_capacity) : disk.partition_capacity
-          lines << "cache_dir aufs #{disk.mounting_point}/squid #{value_for_cache_dir.to_i} 16 256"
-        end
+      total_capacity = cache_disks.collect{|c| c.partition_capacity.to_i}.sum
+      cache_disks.each do |disk|
+        max_value_squid = disk.system? ? MAX_SQUID_ON_SYSTEM_DISK_SIZE : MAX_SQUID_TOTAL_SIZE
+        value_for_cache_dir =  total_capacity >  max_value_squid ? (disk.partition_capacity * max_value_squid / total_capacity) : disk.partition_capacity
+        lines << "cache_dir aufs #{disk.mounting_point}/squid #{value_for_cache_dir.to_i} 16 256"
       end
+    else
+      lines << "cache_dir aufs /var/spool/squid 30000 16 256"
     end
     lines
   end
+
+  # def self.used_for_system
+  #   self.disk_usage("on / ")
+  # end
+
+  # def self.used_for_cache
+  #   hash = {:raid => nil, :devices => []}
+  #   devs = self.disk_usage("/mnt/sequreisp/dev")
+  #   if devs[:devices].empty?
+  #     devs = self.disk_usage("/mnt/cache")
+  #     devs = self.disk_usage("/mnt/cache/web") if devs[:devices].empty?
+  #     hash[:devices] = devs[:devices]
+  #   else
+  #     devs[:devices].each do |dev|
+  #       hash[:devices] << dev if File.directory?("/mnt/sequreisp#{dev}/squid")
+  #     end
+  #   end
+  #   hash[:raid] = devs[:raid]
+  #   hash
+  # end
+
+  # def self.disk_usage(command)
+  #   hash = {:raid => nil, :devices => []}
+  #   IO.popen("mount | grep '#{command}'", "r") do |io|
+  #     io.each do |line|
+  #       device = line.chomp.split(" ").first
+  #       if device.include?("md")
+  #         hash[:raid] = device
+  #         IO.popen("cat /proc/mdstat | grep #{device.split("/").last}", "r") do |io|
+  #           io.each do |line|
+  #             _cache_disks = line.chomp.split(" ")
+  #             _cache_disks[4.._cache_disks.count].each do |disk|
+  #               hash[:devices] << "/dev/#{disk[0..2]}"
+  #             end
+  #           end
+  #         end
+  #       else
+  #         hash[:devices] << device.delete(device.last)
+  #       end
+  #     end
+  #   end
+  #   hash
+  # end
+
+# def self.scan
+  #   disks = {}
+  #   aux =`sudo /usr/bin/lshw -C disk`.strip.split("*-")
+  #   system_disks = used_for_system
+  #   cache_disks = used_for_cache
+
+  #   aux.each do |disk|
+  #     if disk.include?("disk")
+  #       name = ""
+  #       capacity = ""
+  #       serial = ""
+  #       attributes = disk.split("  ")
+  #       attributes.each do |attr|
+  #         name = attr.chomp.split(":").last.strip if attr.include?("logical name")
+  #         capacity = attr.chomp.split(":").last.split(" ").last.strip if attr.include?("size:")
+  #         serial = attr.chomp.split(":").last.strip if attr.include?("serial")
+  #       end
+  #       is_system = system_disks[:devices].include?(name) ? true : false
+  #       is_cache = cache_disks[:devices].include?(name) ? true : false
+  #       #which_raid = system_disks[:raid] if is_system
+  #       #which_raid = cache_disks[:raid] if is_cache
+  #       is_free = is_system or is_cache ? false : true
+  #       partitioned =  is_free ? false : true
+  #       hash = {:name => name, :capacity => capacity, :serial => serial, :system => is_system, :cache => is_cache, :free => is_free, :partitioned => partitioned, :clean_partition => is_free}
+  # #      scan_for_other_uses(hash)
+  #       hash[:raid] = `cat /proc/mdstat | grep "#{hash[:name].split('/').last}"`.chomp.split.first
+  #       disks[name] = hash
+  #     end
+  #   end
+  #   disks
+  # end
+
+  # def self.scaan
+  #   command_disks = `sudo /usr/bin/lshw -C disk`.strip.split("*-")
+  #   command_disks.each do |c_disk|
+  #     name = ""
+  #     capacity = ""
+  #     if c_disk.include?("disk")
+  #       attributes = c_disk.split("  ")
+  #       attributes.each do |attr|
+  #         name = attr.chomp.split(":").last.strip if attr.include?("logical name")
+  #         capacity = attr.chomp.split(":").last.split(" ").last.strip.delete("()") if attr.include?("size:")
+  #       end
+  #       if name.present?
+  #         disk = Disk.find_by_name(name)
+  #         disk = Disk.create(:name => name, :capacity => capacity) if disk.nil?
+  #         disk.raid = disk.which_raid
+  #         disk.is_used_for
+  #         disk.serial = `sudo /sbin/blkid #{disk.partition_name_or_raid_name}`.chomp.split[1].split("=")[1].delete("\"")
+  #         disk.save
+  #       end
+  #     end
+  #   end
+  # end
+
+  # def self.cache_dir_lines
+  #   lines = []
+  #   if Disk.not_custom_raids_present?
+  #     # NO RAIDS, whe take CONTROL
+  #     system "sed -i '/^ *cache_dir*/ c #cache_dir ufs \/var\/spool\/squid 30000 16 256' /etc/squid/squid.conf"
+
+  #     cache_disks = Disk.cache
+  #     if cache_disks.empty?
+  #       #disk = Disk.system.first
+  #       #disk.do_prepare_disk_for_cache
+  #       # disco sistema: hasta 30% y un tope de 50 GB
+  #       value_for_cache_dir = disk.partition_capacity > MAX_SQUID_ON_SYSTEM_DISK_SIZE ? MAX_SQUID_ON_SYSTEM_DISK_SIZE : disk.partition_capacity
+  #       lines << "cache_dir aufs /var/spool/squid #{value_for_cache_dir.to_i} 16 256"
+  #     else
+  #       #system "rm -rf /var/spool/squid &"
+  #       # total_capacity = cache_disks.collect{|c| c.capacity.to_i}.sum / 1024 / 1024
+  #       total_capacity = cache_disks.collect{|c| c.partition_capacity.to_i}.sum
+  #       cache_disks.each do |disk|
+  #         # disco aparte: hasta 30% proporcional al disco hasta un tope de 300GB TOTAL
+  #         value_for_cache_dir =  total_capacity > MAX_SQUID_TOTAL_SIZE ? (disk.partition_capacity * MAX_SQUID_TOTAL_SIZE / total_capacity) : disk.partition_capacity
+  #         lines << "cache_dir aufs #{disk.mounting_point}/squid #{value_for_cache_dir.to_i} 16 256"
+  #       end
+  #     end
+  #   end
+  #   lines
+  # end
 end
