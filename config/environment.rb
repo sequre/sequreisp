5add2a5e4746fc3db1efd148a6060d4d0bef7f80
Luciano Ruete <lruete@sequre.com.ar>
2014-07-10 13:09:02 -0300

    [misc] Fix bad line

--- config/environment.rb
+++ config/environment.rb
@@ -42,5 +42,4 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   config.i18n.default_locale = :en
-  config.i18n.load_path += Dir[Rails.root.join('vendor', 'plugins', '**', 'config', 'locales', '*.yml')].delete_if{|item| item.include?("/sequreisp_invoicing/")}
 end
