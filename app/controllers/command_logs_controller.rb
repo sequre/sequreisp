7b762ae815f7ae6fc72d0af800fff2b0aa2a664a
Enrique Serdio <enriserdio@gmail.com>
2014-11-18 14:54:32 -0300

    [refactor] change command_log view logic

--- app/controllers/command_logs_controller.rb
+++ app/controllers/command_logs_controller.rb
@@ -3,9 +3,9 @@ class CommandLogsController < ApplicationController
   permissions :command_log
 
   def command_log_info
-    command_log_file_info = File.read(Configuration::PATH_COMMANDO_LOG) rescue ""
+    command_log_file_lines = File.open(Configuration::PATH_COMMANDO_LOG, "r").readlines rescue []
     respond_to do |format|
-      format.json { render :json => command_log_file_info }
+      format.json { render :json => {:last_line => (command_log_file_lines.length - 1), :command_log_lines => command_log_file_lines[params[:from_line].to_i..-1] }}
     end
   end
 
