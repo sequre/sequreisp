345b51fc45ebadd08f16507ae3555b4798bf9349
g.edera <gab.edera@gmail.com>
2015-04-20 09:45:49 -0300

    [misc] Cleaning code

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -16,28 +16,33 @@
 # along with Sequreisp.  If not, see <http://www.gnu.org/licenses/>.
 
 class Plan < ActiveRecord::Base
-  has_many :contracts, :dependent => :destroy, :include => :klass
-  has_many :providers, :through => :provider_group
-  acts_as_audited
-  belongs_to :provider_group
 
+  RE_USED = 're_used'
+  PERCENTAGE = 'percentage'
+  TOTAL_CIR = 'total_cir'
+  AUTOMATIC = 'automatic'
+
+  acts_as_audited
   attr_accessor :cir_percentage
   attr_accessor :value_cir_re_used
 
   include ModelsWatcher
-  watch_fields :provider_group_id, :ceil_down, :ceil_up, :total_cir_down, :total_cir_up, :cir_down, :cir_up, :burst_down, :burst_up, :long_download_max, :long_upload_max
+  watch_fields :provider_group_id, :ceil_down, :ceil_up, :total_cir_down, :total_cir_up, :cir_down, :cir_up, :long_download_max, :long_upload_max
+
+  has_many :contracts, :dependent => :destroy, :include => :klass
+  has_many :providers, :through => :provider_group
+  belongs_to :provider_group
 
   validates_uniqueness_of :name
   validates_presence_of :name, :provider_group, :ceil_down, :ceil_up
-  validates_presence_of :value_cir_re_used, :if => "how_use_cir == 're_used'"
-  validates_presence_of :cir_percentage, :if => "how_use_cir == 'percentage'"
-  validates_presence_of :total_cir_down, :total_cir_up, :if => "how_use_cir == 'total_cir'"
+  validates_presence_of :value_cir_re_used, :if => "how_use_cir == #{RE_USED}"
+  validates_presence_of :cir_percentage, :if => "how_use_cir == #{PERCENTAGE}"
+  validates_presence_of :total_cir_down, :total_cir_up, :if => "how_use_cir == #{TOTAL_CIR}"
   validates_length_of :name, :in => 3..128
   validates_numericality_of :ceil_down, :ceil_up, :only_integer => true, :allow_nil => true, :greater_than => 0
-  validates_numericality_of :burst_down, :burst_up, :only_integer => true, :greater_than_or_equal_to => 0
   validates_numericality_of :long_download_max, :long_upload_max, :only_integer => true, :greater_than_or_equal_to => 0, :less_than => 4294967295
 
-  validate :cir_percentage_less_than_and_greater_than, :if => "how_use_cir == 'percentage'"
+  validate :cir_percentage_less_than_and_greater_than, :if => "how_use_cir == #{PERCENTAGE}"
 
   before_save :set_cir_and_total_cir
 
@@ -48,19 +53,19 @@ class Plan < ActiveRecord::Base
 
   def set_cir_and_total_cir
     set_cir
-    set_total_cir if how_use_cir != "total_cir"
+    set_total_cir if how_use_cir != TOTA_CIR
   end
 
   def set_cir
     case how_use_cir
-    when "percentage"
+    when PERCENTAGE
       self.cir_up = self.cir_down = self.cir_percentage
-    when "re_used"
+    when RE_USED
       self.cir_up = self.cir_down = self.value_cir_re_used
-    when "automatic"
-      self.cir_up = provider_group.rate_up / (provider_group.ceil_up) rescue 0.0001
-      self.cir_down = provider_group.rate_down / (provider_group.ceil_down) rescue 0.0001
-    when "total_cir"
+    when AUTOMATIC
+      self.cir_up = provider_group.rate_up / provider_group.ceil_up rescue 0.0001
+      self.cir_down = provider_group.rate_down / provider_group.ceil_down rescue 0.0001
+    when TOTAL_CIR
       self.cir_up = self.total_cir_up / (self.ceil_up * contracts_count) rescue 0.0001
       self.cir_down = self.total_cir_down / (self.ceil_down * contracts_count) rescue 0.0001
     end
@@ -105,12 +110,6 @@ class Plan < ActiveRecord::Base
     cir_up || 1.0
   end
 
-  def burst_down_to_bytes
-    (burst_down / 8) * 1024
-  end
-  def burst_up_to_bytes
-    (burst_up / 8) * 1024
-  end
   def long_download_max_to_bytes
     long_download_max * 1024
   end
