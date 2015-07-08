3f751ce636091f0d7845d2d7fc22d799fd00f6d5
Valentin Sanjuan <vsanjuan@sequreisp.com>
2015-07-08 11:05:13 -0300

    [refactor] device brand crud is now an attribute

--- app/models/device.rb
+++ app/models/device.rb
@@ -6,10 +6,9 @@ class Device < ActiveRecord::Base
   KIND_ROUTER = "router"
   named_scope :aps, :conditions => "kind = KIND_AP"
   named_scope :need_update, lambda {  { :conditions => ["updated_at <= ?", 5.minutes.ago]} }
-  validates_presence_of :host, :kind
+  validates_presence_of :host, :kind, :brand
   validates_uniqueness_of :host
   # validates_format_of :mac_address, :with => /^([0-9A-Fa-f]{2}\:){5}[0-9A-Fa-f]{2}$/, :allow_blank => true
   belongs_to :device
   belongs_to :contract
-  belongs_to :device_brand
 end
--- app/models/device_brand.rb
+++
@@ -1,4 +0,0 @@
-class DeviceBrand < ActiveRecord::Base
-  validates_uniqueness_of :name
-  has_many :devices
-end
