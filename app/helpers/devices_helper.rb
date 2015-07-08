a77961cb3613d01d3c8f7109521bca448b78c1ed
Enrique Serdio <enriserdio@gmail.com>
2015-07-08 10:42:55 -0300

    Revert "[refactor] device brand crud replaced provisionally with attribute"
    This reverts commit dcd2da346bd775ab8d7a1c74482d93c0243244bd.

--- app/helpers/devices_helper.rb
+++ app/helpers/devices_helper.rb
@@ -1,20 +1,11 @@
 module DevicesHelper
   def devices_kinds_for_select
-    [
+    [ 
       [I18n.t("activerecord.attributes.device.kinds.#{Device::KIND_CPE}"), Device::KIND_CPE],
       [I18n.t("activerecord.attributes.device.kinds.#{Device::KIND_AP}"), Device::KIND_AP],
       [I18n.t("activerecord.attributes.device.kinds.#{Device::KIND_SWITCH}"), Device::KIND_SWITCH],
       [I18n.t("activerecord.attributes.device.kinds.#{Device::KIND_SERVER}"), Device::KIND_SERVER],
       [I18n.t("activerecord.attributes.device.kinds.#{Device::KIND_ROUTER}"), Device::KIND_ROUTER]
-    ]
-  end
-
-  def devices_brands_for_select
-    [
-     ["Ubiquiti", "ubiquiti"],
-     ["MikroTik", "mikrotik"],
-     ["Cisco", "cisco"],
-     ["Other", "other"]
     ]
   end
 end
