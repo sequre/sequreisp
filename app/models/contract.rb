a9faf3a566cf9c6c947c3d65f8f83707c2adaeda
g.edera <gab.edera@gmail.com>
2015-09-24 14:15:41 -0300

    minor change

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -392,7 +392,7 @@ class Contract < ActiveRecord::Base
 
     ContractSample.compact_keys.each do |rkey|
       value = if Rails.env.production?
-                date_keys.empty? ? 0 : ($redis.hmget(date_keys.last, "#{rkey[:name]}_instant").first.to_i / $redis.hmget(date_keys.last, "total_seconds").first.to_i) * 8
+                date_keys.empty? ? 0 : ($redis.hget(date_keys.last, "#{rkey[:name]}_instant").to_i / $redis.hget(date_keys.last, "total_seconds").to_i) * 8
               else
                 rand(1024)
               end
