cfc04fc1e6152d657ea0cd58cae384a397c3a29b
g.edera <gab.edera@gmail.com>
2015-10-20 11:03:37 -0300

    Add method log_rescue_file

--- lib/sequreisp_logger.rb
+++ lib/sequreisp_logger.rb
@@ -69,6 +69,19 @@ class ApplicationLogger
 
 end
 
+def log_rescue_file(path, exception)
+  File.open(path, 'a+') do |f|
+    if exception.instance_of? String
+      f.puts DateTime.now
+      f.puts "#{DateTime.now} - #{exception}"
+    else
+      f.puts "#{DateTime.now} - #{exception.message}"
+      exception.backtrace.each{ |bt| f.puts "#{exception.class} #{bt}" }
+    end
+  end
+end
+
+
 # require 'syslog'
 
 # def log(string)
@@ -89,18 +102,5 @@ end
 #   else
 #     Rails.logger.error "[Wispro]#{origin}: #{exception.message}"
 #     exception.backtrace.each{ |bt| Rails.logger.error("[Wispro]#{origin} #{exception.class} #{bt}") }
-#   end
-# end
-
-
-# def log_rescue_file(path, exception)
-#   File.open(path, 'a+') do |f|
-#     if exception.instance_of? String
-#       f.puts DateTime.now
-#       f.puts "#{DateTime.now} - #{exception}"
-#     else
-#       f.puts "#{DateTime.now} - #{exception.message}"
-#       exception.backtrace.each{ |bt| f.puts "#{exception.class} #{bt}" }
-#     end
 #   end
 # end
