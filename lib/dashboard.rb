c0391c0a4a47509318315fbf7bd94224aef079f9
Enrique Serdio <enriserdio@gmail.com>
2015-12-15 13:57:17 -0300

    [misc][gui] change style of services table on dashboard

--- lib/dashboard.rb
+++ lib/dashboard.rb
@@ -2,7 +2,6 @@ module Dashboard
   SERVICES= [
     { :name => I18n.t("dashboard.name_service.sequreisp_daemon"), :pattern => 'sequreispd.rb ' },
     { :name => I18n.t("dashboard.name_service.bind_dns_server"), :pattern => 'named'},
-    { :name => I18n.t("dashboard.name_service.apache_web_server"), :pattern => 'apache2' },
     { :name => I18n.t("dashboard.name_service.mysql_server"), :pattern => 'mysqld' },
     { :name => I18n.t("dashboard.name_service.redis_server"), :pattern => 'redis-server' },
     { :name => I18n.t("dashboard.name_service.sequreisp_redis_process"), :pattern => 'sequreispd_daemon_redis ' },
