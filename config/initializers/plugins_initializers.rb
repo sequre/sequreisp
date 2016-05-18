e7987f4843a72194f9b9bb8ed07abbb6b179dced
g.edera <gab.edera@gmail.com>
2016-05-18 11:44:09 -0300

    require path migration

--- config/initializers/plugins_initializers.rb
+++ config/initializers/plugins_initializers.rb
@@ -1,3 +1,7 @@
 Dir.glob(File.join(Rails.root, 'vendor', 'plugins', '**', 'config', 'initializers', '*')) do |initializer|
-    self.send(:eval, File.open(initializer, 'r').read)
+  self.send(:eval, File.open(initializer, 'r').read)
+end
+
+Dir.glob(File.join(Rails.root, 'vendor', 'plugins', '**', 'db', 'migrate', '*')) do |path|
+  require path
 end
