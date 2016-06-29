1992c717d23a23f6dfc5d13306e88d42bda984c0
g.edera <gab.edera@gmail.com>
2016-06-29 09:11:14 -0300

    Avoid the uniqueness client name

--- app/models/client.rb
+++ app/models/client.rb
@@ -23,8 +23,8 @@ class Client < ActiveRecord::Base
   validates_presence_of :name
   validates_length_of :name, :in => 3..128
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :allow_blank => true
-  validates_uniqueness_of :name
-  #validates_uniqueness_of :email, :allow_blank => true
+  # validates_uniqueness_of :name
+  # validates_uniqueness_of :email, :allow_blank => true
 
   named_scope :number_client_like, lambda { |number|
     if Configuration.first.use_external_number_client;
