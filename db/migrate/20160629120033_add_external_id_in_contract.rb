8a70a29d35633450454dfe4b4859420a122d7201
Enrique Serdio <enriserdio@gmail.com>
2016-06-29 09:49:57 -0300

    fix migration for external_id in contract

--- db/migrate/20160629120033_add_external_id_in_contract.rb
+++ db/migrate/20160629120033_add_external_id_in_contract.rb
@@ -1,9 +1,9 @@
 class AddExternalIdInContract < ActiveRecord::Migration
   def self.up
-    add_column :clients, :external_id, :integer, :index => true
+    add_column :contracts, :external_id, :integer, :index => true
   end
 
   def self.down
-    remove_column :clients, :external_id
+    remove_column :contracts, :external_id
   end
 end
