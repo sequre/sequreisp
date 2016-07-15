215994999a2eaa2f74f6ead4d196d6b3a8976b9e
g.edera <gab.edera@gmail.com>
2016-07-15 13:56:31 -0300

    Add boot hook after in filter

--- lib/sequreisp.rb
+++ lib/sequreisp.rb
@@ -510,6 +510,7 @@ def gen_iptables
           f.puts "-A enabled-MAIN -j DROP"
         end
       end
+      BootHook.run(:hook => :filter_after_all, :iptables_script => f) unless Configuration.in_safe_mode?
       f.puts "COMMIT"
       #---------#
       # /FILTER #
