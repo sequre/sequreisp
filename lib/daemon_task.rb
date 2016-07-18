9685dba0d7e455953cb2c19f2867ee417822a8c6
g.edera <gab.edera@gmail.com>
2016-07-18 14:09:11 -0300

    minor change

--- lib/daemon_task.rb
+++ lib/daemon_task.rb
@@ -653,7 +653,6 @@ end
 class DaemonCompactSamples < DaemonTask
 
   def initialize
-<<<<<<< HEAD
     @time_for_exec = { }
     @wait_for_apply_changes = true
     @proc = Proc.new { exec_daemon_compact_samples }
